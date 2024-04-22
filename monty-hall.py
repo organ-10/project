35c562040b7a6fbf7ce6708335c36d610edcc88c
pybro11 <pybro2816@gmail.com>
2024-04-22 17:10:13 +0900

    make file: mony-hall.py
    몬티홀 문제 시뮬레이션 해결했습니다.

---
+++ monty-hall.py
@@ -0,0 +1,18 @@
+import random
+
+stay = 0
+change = 0
+
+tri = int(input("시행 횟수를 입력해 주세요: "))
+
+for i in range(tri):
+    doors = [0, 0, 1]
+    random.shuffle(doors)
+    user_choice = doors.pop()
+    doors.remove(0)
+    if user_choice == 1:
+        stay+=1
+    else:
+        change+=1
+
+print(f"stay: {stay}, change: {change}")
