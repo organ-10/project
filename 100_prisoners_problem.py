cde4cb3259d0c873ff4b6115343db8d2ba25b789
Dongjae Heo <lindsay.heo@gmail.com>
2024-04-22 16:55:08 +0900

    feat: add 100 prisoners problem

---
+++ 100_prisoners_problem.py
@@ -0,0 +1,9 @@
+def prisoner_problem():
+    import random
+    import numpy as np
+    n = 100
+    prisoners = list(range(1, n+1))
+    picked_prisoners = set()
+    print(type(picked_prisoners))
+
+prisoner_problem()
